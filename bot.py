114653470c24ff0085fe776cfa8dba0b125976e0
GrayFennec <tapniltap@gmail.com>
2021-07-26 15:30:31 -0700

    added weather command

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 # bot.py
-# bot.py
 import os
 import random
 import discord
 import math
+import requests
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
+WEATHER_KEY = os.getenv('WEATHER_KEY')
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
@@ -43,4 +43,31 @@ async def roll(ctx, accuracy_difficulty: int, modifier: int):
     ]
     await ctx.send('\n'.join(response))
 
+@bot.command(name='weather', help='Gets the weather of one of the habitable moons.\nCurrently supported: Ari, Bay, Cole, Drop')
+async def weather(ctx, moon_name):
+    moon_city_dictionary = {
+        'Ari': 'Riyadh',
+        'Bay': 'San Francisco',
+        'Cole': 'Svalbard',
+        'Drop': 'Singapore',
+    }
+    try:
+        city_name = moon_city_dictionary[moon_name]
+    except KeyError:
+        await ctx.send('Error: Unsupported moon name.')
+        return
+    param_set = {'key':WEATHER_KEY,'q':city_name,'aqi':'no'}
+    response = requests.get('http://api.weatherapi.com/v1/current.json', params = param_set)
+    weather_data = response.json()['current']
+    weather_text = [
+        '__' + moon_name + ' weather__',
+        weather_data['condition']['text'],
+        'Temperature: ' + str(weather_data['temp_f']) + '°F',
+        'Precipitation: ' + str(weather_data['precip_in']) + 'in',
+        'Humidity: ' + str(weather_data['humidity']) + '%',
+        'Wind: ' + str(weather_data['wind_mph']) + ' mph ' + weather_data['wind_dir'],
+        'Air Pressure: ' + str(weather_data['pressure_in']) + ' in',
+        'Cloud Cover: ' + str(weather_data['cloud']) + '%'
+    ]
+    await ctx.send('\n'.join(weather_text))
 bot.run(TOKEN)
